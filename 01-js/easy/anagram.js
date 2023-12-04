b4f533168d69b5bb28632fce19c58fe6dc4db96a
Arpan <93578947+arpangarg05@users.noreply.github.com>
2023-12-04 15:20:52 +0530

    Add files via upload

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,20 @@
 
 function isAnagram(str1, str2) {
 
+  if(str1.length == str2.length){
+    if(str1.length == 0){return true;}
+    for(let i=0;i<str1.length;i++){
+      let flagg = false;
+      for(let j=0;j<str1.length;j++){
+        if(str1[i].toLowerCase() == str2[j].toLowerCase()){
+          flagg = true;
+        }
+      }
+      if (flagg==true){ return true;}
+      else return false;
+    }
+  }
+  return false;
 }
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,68 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  if(transactions.length==0)return [];
+
+  let output = [];
+  output.push({'category':transactions[0].category,'totalSpent':transactions[0].price});
+  for(let i=1;i<transactions.length;i++){
+    let ink = transactions[i].category;
+    let flag = false;
+    for(let j=0;j<output.length;j++){
+      if(output[j]['category'] == ink){
+        output[j]['totalSpent'] +=transactions[i].price;
+        flag = true;
+      }
+    }
+    if(flag == false){
+      output.push({'category':transactions[i].category,'totalSpent':transactions[i].price});
+    }
+    
+  }
+  return output;
 }
 
+
+
+
+
+
+let k = calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+]);
+console.log(k);
 module.exports = calculateTotalSpentByCategory;
